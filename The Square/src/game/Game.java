c1ecaa2033e691686e5c59109836be09bf24c8c8
Oliver <oliverschall22@googlemail.com>
2016-05-07 01:54:41 +0200

    Improved movement, added bounds to screen sides

--- The Square/src/game/Game.java
+++ The Square/src/game/Game.java
@@ -1,10 +1,5 @@
 package game;
 
-import broadphase.SAP;
-import display.DisplayMode;
-import display.GLDisplay;
-import display.PixelFormat;
-import display.VideoSettings;
 import gui.Font;
 import input.Input;
 import input.InputEvent;
@@ -16,6 +11,7 @@ import manifold.SimpleManifoldManager;
 import math.VecMath;
 import narrowphase.EPA;
 import narrowphase.GJK;
+import objects.GhostObject3;
 import objects.RigidBody3;
 import physics.PhysicsDebug;
 import physics.PhysicsShapeCreator;
@@ -39,6 +35,12 @@ import utils.ProjectionHelper;
 import utils.SimpleGameProfiler;
 import vector.Vector2f;
 import vector.Vector3f;
+import broadphase.SAP;
+import collisionshape.CylinderShape;
+import display.DisplayMode;
+import display.GLDisplay;
+import display.PixelFormat;
+import display.VideoSettings;
 
 public class Game extends StandardGame {
 	Debugger debugger;
@@ -48,45 +50,77 @@ public class Game extends StandardGame {
 
 	Cylinder player;
 	RigidBody3 playerbody;
+	GhostObject3 playerJumpChecker;
+	boolean onGround;
 	InputEvent up, down, left, right, jump;
 	final Vector3f vecUp = new Vector3f(0, 1, 0);
 	final Vector3f vecRight = new Vector3f(0, 0, -1);
 	final Vector2f vecX = new Vector2f(1, 0);
 	Quad intersectionInterface;
-	
-	final float playerMoveSpeedX = 4f;
-	final float playerMoveSpeedZ = 6f;
+
+	final Vector2f SCREEN_MIN = new Vector2f(-6, -5);
+	final Vector2f SCREEN_MAX = new Vector2f(6, 5);
+
+	final Vector2f PLAYER_SIZE = new Vector2f(0.5f, 0.5f);
+	final float PLAYER_MOVE_SPEED_X = 4f;
+	final float PLAYER_MOVE_SPEED_Z = 4f;
+	final float PLAYER_CHECKER_SIZE = 0.1f;
+	final float PLAYER_CHECKER_OFFSET = PLAYER_CHECKER_SIZE + 0.1f;
+	final float PLAYER_CHECKER_SIDE_OFFSET = 0.02f;
+	final float PLAYER_JUMP_STRENGTH = 6f;
+	final float PLAYER_MAX_Y = SCREEN_MAX.y - PLAYER_SIZE.y;
+	final float PLAYER_MAX_DIST_TO_CENTER = SCREEN_MAX.x - PLAYER_SIZE.x;
+	final float PLAYER_MAX_DIST_TO_CENTER_SQUARED = PLAYER_MAX_DIST_TO_CENTER
+			* PLAYER_MAX_DIST_TO_CENTER;
+
+	int millisSinceLastJump = 0;
+	final int MILLIS_BETWEEN_JUMPS = 100;
 
 	@Override
 	public void init() {
 		useFBO = false;
 		initDisplay(new GLDisplay(), new DisplayMode(), new PixelFormat(),
-				new VideoSettings(DefaultValues.DEFAULT_VIDEO_RESOLUTION_X, DefaultValues.DEFAULT_VIDEO_RESOLUTION_Y,
-						DefaultValues.DEFAULT_VIDEO_FOVY, DefaultValues.DEFAULT_VIDEO_ZNEAR,
+				new VideoSettings(DefaultValues.DEFAULT_VIDEO_RESOLUTION_X,
+						DefaultValues.DEFAULT_VIDEO_RESOLUTION_Y,
+						DefaultValues.DEFAULT_VIDEO_FOVY,
+						DefaultValues.DEFAULT_VIDEO_ZNEAR,
 						DefaultValues.DEFAULT_VIDEO_ZFAR, false),
 				new NullSoundEnvironment());
-		layer3d.setProjectionMatrix(ProjectionHelper.ortho(6, -6, -5, 5, -1, 1));
+		layer3d.setProjectionMatrix(ProjectionHelper.ortho(SCREEN_MAX.x,
+				SCREEN_MIN.x, SCREEN_MIN.y, SCREEN_MAX.y, -1, 1));
 		display.bindMouse();
 		cam.setFlyCam(false);
 		cam.translateTo(0f, 0f, 0);
 		cam.rotateTo(0, 0);
 
-		Shader defaultshader = new Shader(
-				ShaderLoader.loadShaderFromFile("res/shaders/defaultshader.vert", "res/shaders/defaultshader.frag"));
+		Shader defaultshader = new Shader(ShaderLoader.loadShaderFromFile(
+				"res/shaders/defaultshader.vert",
+				"res/shaders/defaultshader.frag"));
 		addShader(defaultshader);
 		Shader defaultshaderInterface = new Shader(
-				ShaderLoader.loadShaderFromFile("res/shaders/defaultshader.vert", "res/shaders/defaultshader.frag"));
+				ShaderLoader.loadShaderFromFile(
+						"res/shaders/defaultshader.vert",
+						"res/shaders/defaultshader.frag"));
 		addShaderInterface(defaultshaderInterface);
 
-		Input inputKeyW = new Input(Input.KEYBOARD_EVENT, "W", KeyInput.KEY_DOWN);
-		Input inputKeyA = new Input(Input.KEYBOARD_EVENT, "A", KeyInput.KEY_DOWN);
-		Input inputKeyS = new Input(Input.KEYBOARD_EVENT, "S", KeyInput.KEY_DOWN);
-		Input inputKeyD = new Input(Input.KEYBOARD_EVENT, "D", KeyInput.KEY_DOWN);
-		Input inputKeySpace = new Input(Input.KEYBOARD_EVENT, "Space", KeyInput.KEY_DOWN);
-		Input inputKeyUp = new Input(Input.KEYBOARD_EVENT, "Up", KeyInput.KEY_DOWN);
-		Input inputKeyLeft = new Input(Input.KEYBOARD_EVENT, "Left", KeyInput.KEY_DOWN);
-		Input inputKeyDown = new Input(Input.KEYBOARD_EVENT, "Down", KeyInput.KEY_DOWN);
-		Input inputKeyRight = new Input(Input.KEYBOARD_EVENT, "Right", KeyInput.KEY_DOWN);
+		Input inputKeyW = new Input(Input.KEYBOARD_EVENT, "W",
+				KeyInput.KEY_DOWN);
+		Input inputKeyA = new Input(Input.KEYBOARD_EVENT, "A",
+				KeyInput.KEY_DOWN);
+		Input inputKeyS = new Input(Input.KEYBOARD_EVENT, "S",
+				KeyInput.KEY_DOWN);
+		Input inputKeyD = new Input(Input.KEYBOARD_EVENT, "D",
+				KeyInput.KEY_DOWN);
+		Input inputKeySpace = new Input(Input.KEYBOARD_EVENT, "Space",
+				KeyInput.KEY_DOWN);
+		Input inputKeyUp = new Input(Input.KEYBOARD_EVENT, "Up",
+				KeyInput.KEY_DOWN);
+		Input inputKeyLeft = new Input(Input.KEYBOARD_EVENT, "Left",
+				KeyInput.KEY_DOWN);
+		Input inputKeyDown = new Input(Input.KEYBOARD_EVENT, "Down",
+				KeyInput.KEY_DOWN);
+		Input inputKeyRight = new Input(Input.KEYBOARD_EVENT, "Right",
+				KeyInput.KEY_DOWN);
 		up = new InputEvent("Up", inputKeyW, inputKeyUp);
 		down = new InputEvent("Down", inputKeyS, inputKeyDown);
 		left = new InputEvent("Left", inputKeyA, inputKeyLeft);
@@ -98,12 +132,14 @@ public class Game extends StandardGame {
 		inputs.addEvent(right);
 		inputs.addEvent(jump);
 
-		space = new PhysicsSpace(new VerletIntegration(), new SAP(), new GJK(new EPA()), new ImpulseResolution(),
-				new ProjectionCorrection(0.01f), new SimpleManifoldManager<Vector3f>());
+		space = new PhysicsSpace(new VerletIntegration(), new SAP(), new GJK(
+				new EPA()), new ImpulseResolution(), new ProjectionCorrection(
+				0.01f), new SimpleManifoldManager<Vector3f>());
 		space.setGlobalGravitation(new Vector3f(0, -8f, 0));
 
 		Font font = FontLoader.loadFont("res/fonts/DejaVuSans.ttf");
-		debugger = new Debugger(inputs, defaultshader, defaultshaderInterface, font, cam);
+		debugger = new Debugger(inputs, defaultshader, defaultshaderInterface,
+				font, cam);
 		physicsdebug = new PhysicsDebug(inputs, font, space);
 		GameProfiler gp = new SimpleGameProfiler();
 		setProfiler(gp);
@@ -111,20 +147,33 @@ public class Game extends StandardGame {
 		space.setProfiler(pp);
 		profiler = new Profiler(this, inputs, font, gp, pp);
 
-		Cylinder ground = new Cylinder(0, -5, 0, 5, 1, 36);
-		RigidBody3 rb = new RigidBody3(PhysicsShapeCreator.create(ground));
-		space.addRigidBody(ground, rb);
-		defaultshader.addObject(ground);
-
-		player = new Cylinder(0, 0, 0, 1, 1, 36);
+		player = new Cylinder(0, 0, 0, PLAYER_SIZE.x, PLAYER_SIZE.y, 36);
 		playerbody = new RigidBody3(PhysicsShapeCreator.create(player));
 		playerbody.setMass(1f);
+		playerbody.setAngularFactor(new Vector3f(0, 0, 0));
+		playerbody.setRestitution(0);
 		space.addRigidBody(player, playerbody);
 		defaultshader.addObject(player);
 
+		playerJumpChecker = new GhostObject3(
+				new CylinderShape(0, 0, 0, PLAYER_SIZE.x
+						- PLAYER_CHECKER_SIDE_OFFSET, PLAYER_CHECKER_SIZE));
+		playerJumpChecker.setMass(1f);
+		playerJumpChecker.setLinearFactor(new Vector3f(0, 0, 0));
+		playerJumpChecker.setAngularFactor(new Vector3f(0, 0, 0));
+		playerJumpChecker.setRestitution(0);
+		space.addGhostObject(playerJumpChecker);
+
+		Box box = new Box(4, -4, 4, 1, 0.3f, 1);
+		RigidBody3 rb1 = new RigidBody3(PhysicsShapeCreator.create(box));
+		space.addRigidBody(box, rb1);
+		defaultshader.addObject(box);
+
+		onGround = false;
+
 		defaultshader.addObject(new Sphere(0, 2, 0, 1, 36, 36));
 		defaultshader.addObject(new Box(0, 4, 0, 1, 1, 1));
-		
+
 		defaultshaderInterface.addObject(new Circle(55, 55, 50, 36));
 		intersectionInterface = new Quad(55, 55, 55, 1);
 		defaultshaderInterface.addObject(intersectionInterface);
@@ -166,62 +215,100 @@ public class Game extends StandardGame {
 
 		Vector3f frontVec = VecMath.transformVector(cam.getMatrix(), vecRight);
 		Vector3f rightVec = VecMath.crossproduct(frontVec, vecUp);
-		Vector2f move = new Vector2f();
-		Vector2f pos = new Vector2f(playerbody.getTranslation().x, playerbody.getTranslation().z);
+		Vector3f move = new Vector3f();
+		Vector2f pos = new Vector2f(playerbody.getTranslation().x,
+				playerbody.getTranslation().z);
 		boolean isPlayerNotInCenter = pos.lengthSquared() > 0.01f;
-		
-		if(isPlayerNotInCenter) {
+
+		if (isPlayerNotInCenter) {
 			float distToMid = (float) pos.length();
 			if (up.isActive()) {
-				move.x += frontVec.x * distToMid * playerMoveSpeedZ;
-				move.y += frontVec.z * distToMid * playerMoveSpeedZ;
+				move.x += frontVec.x * distToMid * PLAYER_MOVE_SPEED_Z;
+				move.z += frontVec.z * distToMid * PLAYER_MOVE_SPEED_Z;
 			}
 			if (down.isActive()) {
-				move.x -= frontVec.x * distToMid * playerMoveSpeedZ;
-				move.y -= frontVec.z * distToMid * playerMoveSpeedZ;
+				move.x -= frontVec.x * distToMid * PLAYER_MOVE_SPEED_Z;
+				move.z -= frontVec.z * distToMid * PLAYER_MOVE_SPEED_Z;
 			}
-			if(up.isActive() || down.isActive()) {
-				float movespeed = (float) move.length();
-				Vector2f predictedPos = VecMath.addition(pos, VecMath.scale(move, delta / 1000f));
+			if (up.isActive() || down.isActive()) {
+				float movespeed = (float) new Vector2f(move.x, move.z).length();
+				Vector2f predictedPos = VecMath.addition(pos, new Vector2f(
+						move.x * delta / 1000f, move.z * delta / 1000f));
 				predictedPos.normalize();
 				predictedPos.scale(distToMid);
-				move = VecMath.subtraction(predictedPos, pos);
+				move.x = predictedPos.x - pos.x;
+				move.z = predictedPos.y - pos.y;
 				move.setLength(movespeed);
 			}
 		}
-		
+
 		if (left.isActive()) {
-			move.x += rightVec.x * playerMoveSpeedX;
-			move.y += rightVec.z * playerMoveSpeedX;
+			move.x += rightVec.x * PLAYER_MOVE_SPEED_X;
+			move.z += rightVec.z * PLAYER_MOVE_SPEED_X;
 		}
 		if (right.isActive()) {
-			move.x -= rightVec.x * playerMoveSpeedX;
-			move.y -= rightVec.z * playerMoveSpeedX;
+			move.x -= rightVec.x * PLAYER_MOVE_SPEED_X;
+			move.z -= rightVec.z * PLAYER_MOVE_SPEED_X;
 		}
-		if(jump.isActive()) {
-			// TODO: jump!
+		if (millisSinceLastJump < MILLIS_BETWEEN_JUMPS) {
+			millisSinceLastJump += delta;
+		}
+		if (jump.isActive() && onGround
+				&& millisSinceLastJump >= MILLIS_BETWEEN_JUMPS) {
+			move.y += PLAYER_JUMP_STRENGTH;
+			millisSinceLastJump = 0;
 		}
 
-		playerbody.getLinearVelocity().x = move.x;
-		playerbody.getLinearVelocity().z = move.y;
+		playerbody.setLinearVelocity(new Vector3f(move.x, playerbody
+				.getLinearVelocity().y + move.y, move.z));
+
+		playerJumpChecker.setTranslation(new Vector3f(playerbody
+				.getTranslation().x, playerbody.getTranslation().y
+				- PLAYER_SIZE.y - PLAYER_CHECKER_OFFSET, playerbody
+				.getTranslation().z));
 
 		space.update(delta);
 
+		onGround = space.hasCollision(playerJumpChecker);
+
 		// update pos after physics update
-		pos = new Vector2f(playerbody.getTranslation().x, playerbody.getTranslation().z);
-		isPlayerNotInCenter = pos.lengthSquared() > 0.01f;
-		if((up.isActive() || down.isActive()) && isPlayerNotInCenter) {
+		pos = new Vector2f(player.getTranslation().x, player.getTranslation().z);
+		float distToCenterSquared = (float) pos.lengthSquared();
+		if (distToCenterSquared > PLAYER_MAX_DIST_TO_CENTER_SQUARED) {
+			System.out.println(distToCenterSquared);
+			pos.setLength(PLAYER_MAX_DIST_TO_CENTER);
+			System.out.println(distToCenterSquared);
+			player.getTranslation().x = pos.x;
+			player.getTranslation().z = pos.y;
+		}
+		System.out.println(playerbody.getTranslation().y);
+		if (player.getTranslation().y > PLAYER_MAX_Y) {
+			player.getTranslation().y = PLAYER_MAX_Y;
+			playerbody.getLinearVelocity().y = 0;
+		}
+		if (player.getTranslation().y < -PLAYER_MAX_Y) {
+			player.getTranslation().y = -PLAYER_MAX_Y;
+			playerbody.getLinearVelocity().y = 0;
+			onGround = true;
+		}
+
+		isPlayerNotInCenter = distToCenterSquared > 0.01f;
+		if (isPlayerNotInCenter) {
 			pos.normalize();
-			if(VecMath.dotproduct(pos, new Vector2f(rightVec.x, rightVec.z)) < 0) pos.negate();
+			if (VecMath.dotproduct(pos, new Vector2f(rightVec.x, rightVec.z)) < 0)
+				pos.negate();
 			float dotX = VecMath.dotproduct(pos, vecX);
 			float angleX = (float) Math.toDegrees(Math.acos(dotX));
 			float angle = 0;
-			if(pos.y < 0) angle = angleX;
-			else angle = 180 + (180 - angleX);
+			if (pos.y < 0)
+				angle = angleX;
+			else
+				angle = 180 + (180 - angleX);
 			cam.rotateTo(angle, 0);
 			intersectionInterface.rotateTo(angle);
 		}
-		
+
+		player.updateBuffer();
 		cam.updateBuffer();
 	}
 }
